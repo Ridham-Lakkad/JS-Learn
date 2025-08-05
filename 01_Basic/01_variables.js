bd8e1e07d888b010b34d685fd13107df76d3ee9f
Ridham-Lakkad <ridhamlakkad07@gmail.com>
2025-08-05 18:31:36 +0000

    container config

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountID =125455
+let accountEmail="Ridham@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=5213  This is not allowed  in const
+
+accountEmail="Rl@Rl.com"
+accountPassword="54321"
+accountCity="Gondal"
+
+console.log(accountID)
+console.table([accountID,accountEmail,accountCity,accountPassword])
+
+/*
+we dont use var to declare variable as it occurs in block scope and functional scope
+*/
