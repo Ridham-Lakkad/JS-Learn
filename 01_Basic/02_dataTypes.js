64efd49ec2b2fa66528cab951df3d5f881d105ed
Ridham-Lakkad <ridhamlakkad07@gmail.com>
2025-08-05 18:50:47 +0000

    datatypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"user strict"; //treat as newer version of JS 
+
+// alert(3+3) //we are using nodeJS ,we are not using browser so it dont work in node.
+
+console.log(3
+    +
+    3 )   // code readbility should be high
+
+    // documentation of javaScript should be find in ECMAscript   and mdn
+
+
+let name="Ridham"  //string datatype
+let age=20;
+let isLoggedIn = true ; //bool datatype
+
+//number range => 2 to the power 53
+//bigint
+//string => ""
+//Boolean => true or false
+//null => stand alone value 
+//undefined => value is not assigned
+//symbol => unique 
+
+//object
+
+console.log(typeof undefined)  // it return output as undefined datatype 
+console.log(typeof null)   // it return output as object datatype 
