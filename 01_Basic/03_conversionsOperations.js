d06702daaf3669b8b579f3192a653ee9d18d2f89
Ridham-Lakkad <ridhamlakkad07@gmail.com>
2025-08-06 04:29:49 +0000

    Conversionns of dataTypes

---
+++ 01_Basic/03_conversionsOperations.js
@@ -0,0 +1,30 @@
+let score ="33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//when it is not a number then also valueInNumber give data type number 
+//value it gives NAN(not a number )
+// For null it gives 0
+//For undefined it gives NAN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false 
+// "" => false ; 
+//"ridham" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber );
+console.log(stringNumber);
+console.log (typeof stringNumber);
+
