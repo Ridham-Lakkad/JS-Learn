e1a5878fb7a9c908e3c3a7c54d09aebb9c7a817b
Ridham-Lakkad <ridhamlakkad07@gmail.com>
2025-08-06 04:55:53 +0000

    conversions of dataTypes

--- 01_Basic/03_conversionsOperations.js
+++ 01_Basic/03_conversionsOperations.js
@@ -1,11 +1,11 @@
 let score ="33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber =Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 //when it is not a number then also valueInNumber give data type number 
@@ -16,7 +16,7 @@ console.log(valueInNumber)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false 
 // "" => false ; 
@@ -26,5 +26,45 @@ let someNumber = 33
 
 let stringNumber = String(someNumber );
 console.log(stringNumber);
-console.log (typeof stringNumber);
+// console.log (typeof stringNumber);
+
+     // ***********operations***********//
+
+
+let value = 3
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello";
+let str2=" Ridham";
+
+let str3 = str1+ str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// for more detials for above consvertions we can see in ECMAscript
+
+// console.log(+true) // give ans 1;  but true+ is not possible
+// console.log(+"")  // give ans 0;
+
+let gameCounter = 100
+gameCouneter++;   //check what is differ in prefix and postfix increment
+// prefix and postfix  js mdn 
+
+console.log(gameCounter);
+
+
+
 
