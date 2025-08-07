f77b4cb3e526e6c11b8bf11b98d9da09c2aec144
Ridham-Lakkad <ridhamlakkad07@gmail.com>
2025-08-07 15:23:48 +0000

    04_comparision

---
+++ 01_Basic/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("2" > 1);//true
+//console.log("2" >= 1);//true
+
+console.log(null > 0);// false
+console.log(null == 0);  //false
+console.log(null >=0);  //true it converts null into 0 in comparision
+
+// because equality check  ==  and comparision < > <= >=  work differently
+// give such ans is unperdictable
